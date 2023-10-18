649abace7d1922c99eb7dbce6bcf6a2cb5b52701
bayembacke221 <mbackembaye74@gmail.com>
2023-10-18 01:48:27 +0000

    add eureka discovery service

--- service-inscription/src/main/java/com/example/serviceinscription/ServiceInscriptionApplication.java
+++ service-inscription/src/main/java/com/example/serviceinscription/ServiceInscriptionApplication.java
@@ -2,8 +2,10 @@ package com.example.serviceinscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceInscriptionApplication {
 
     public static void main(String[] args) {
