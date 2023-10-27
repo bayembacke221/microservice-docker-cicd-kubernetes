5b9247bd1dea4dc7e66862ca4385fef5b2457a23
bayembacke221 <mbackembaye74@gmail.com>
2023-10-27 13:03:17 +0000

    add component admission

--- src/app/component/service-admission/service-admission.component.ts
+++ src/app/component/service-admission/service-admission.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ApiAdmissionService} from "../../service/api-admission.service";
+import Admis from "../../model/Admis";
 
 @Component({
   selector: 'app-service-admission',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./service-admission.component.css']
 })
 export class ServiceAdmissionComponent {
+  resultatClasse: any = {
+    'absent': 'bg-primary',
+    'non-admis': 'bg-danger',
+    'admis': 'bg-success'
+  };
+  listeAdmission: any = new Array<Admis>();
+  constructor(private apiAdmissionService: ApiAdmissionService) { }
+
+  ngOnInit(): void {
+    this.apiAdmissionService.getListeAdmission().subscribe((results) => {
+      this.listeAdmission = results;
+    })
+  }
+
+  getClasse(typeResultat: string) {
+    return this.resultatClasse[typeResultat]
+  }
 
 }
